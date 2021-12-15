99ceaa216f89a17224ef8284ee57564bc7ce6dc8
JustasPr <prasukas.j@gmail.com>
2021-12-15 18:17:15 +0200

    todo application

--- script.js
+++ script.js
@@ -1,59 +1,81 @@
 const inputField = document.querySelector('.form-control');
-const tasks = document.querySelector('.todo__list');
+const list = document.querySelector('.todo__list');
 const check = document.querySelector('.fa-check');
-const done = document.querySelector('.done__list');
+const deleteT = document.getElementById('fa-times');
+const clear = document.querySelector('.clear');
+const LINE_THROUGH = "lineThrough";
 
-let list = [], id = 0;
-let listDone = [];
+let tasks = [], id;
+
 let d = new Date();
 let day = d.getUTCDay();
 let month = d.getUTCMonth();
 let year = d.getUTCFullYear();
+let date = `${day}/${month}/${year}`;
 
-window.onload = () => {
-    let data = localStorage.getItem("Todo");
-
-    if(data){
-        list = JSON.parse(data);
-        id = list.length; 
-        loadTodo(list); 
-    }
-    else{
-        list = [];
-        id = 0;
-    }
-    function loadTodo(array){
-        array.forEach(function(item){
-            addTodo(item.name, item.id, item.done, item.date);
-        });
-    }
-};
-inputField.addEventListener("keyup", (e) => {
-    if (e.keyCode === 13) {
-        inputValue = inputField.value;
-        let date = `${day}/${month}/${year}`;
-        if(inputValue) {
-            addTodo(inputValue, id, false, date);
-            list.push(
-                {
-                    name: inputValue,
-                    id: id,
-                    done: false,
-                    date: date
-                }
-            );
-            inputValue.value = "";
-            id++;
-            localStorage.setItem("Todo", JSON.stringify(list));
-        }
-    }
+let data = localStorage.getItem("Todo");
+if(data) {
+    tasks = JSON.parse(data);
+    id = tasks.length;
+    loadTasks(tasks);
+} else {
+    tasks=[]; 
+    id=0;
+}
+function loadTasks(array)
+{
+    array.forEach(function(item){
+        addTask(item.task, item.id, item.date, item.trash);
+    });
+}
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
 });
-const addTodo = (inputValue, id, done, date) => {
+function addTask(inputValue, id, date, trash) {
+    if(trash) {return;}
     let task = `      
     <div class="task">
         <p id="task__name">${inputValue}</p>
         <i class="fas fa-check" id="${id}"></i>
         <p id="task__date">${date}</p>
+        <i class="fas fa-times" id="${id}"></i>
     </div>`;
-    tasks.insertAdjacentHTML('beforeend', task);
+    list.insertAdjacentHTML('beforeend', task);
 }
+inputField.addEventListener('keyup',function(e) {
+    validate();
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        let inputValue = inputField.value;
+        if(inputValue) {
+            addTask(inputValue, id, date, false);
+            tasks.push({
+                task: inputValue,
+                id: id,
+                date: date,
+                trash:false
+            });
+            localStorage.setItem("Todo", JSON.stringify(tasks));
+            id++;
+        }
+        inputField.value = "";
+    }
+});
+list.addEventListener('click', (e) => {
+    const element = e.target;
+    if(element.classList.contains('fa-times')) {
+        if(confirm("Do you really want to delete it?")) {
+            element.parentNode.parentNode.removeChild(element.parentNode);
+            tasks[element.id].trash = true;
+        }
+    }
+    if(element.classList.contains('fa-check')) {
+        const element = e.target;
+        element.parentNode.querySelector("#task__name").classList.toggle(LINE_THROUGH);
+    }
+    localStorage.setItem("Todo", JSON.stringify(tasks));
+});
+function validate() {
+    inputField.value = inputField.value.replace(/[^a-zA-Z0-9@]+/, '');
+};
